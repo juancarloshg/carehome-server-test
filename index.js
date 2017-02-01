165b01cf7cd63730c0dc460d7105dd0959aefb85
Juan Carlos Hernández <Juan Carlos Hernndez>
2017-02-01 22:48:54 +0000

    team imgs fix

--- index.js
+++ index.js
@@ -9,8 +9,19 @@ app.use(express.static(staticRoot));
 var routes = require('./apiRoutes.js')
 app.use('/api', routes);
 
-app.use(function(req, res, next){
-  fs.createReadStream(staticRoot + 'index.html').pipe(res);
+app.use(function(req, res, next){  
+    // if the request is not html then move along
+    var accept = req.accepts('html', 'json', 'xml');
+    if(accept !== 'html'){
+        return next();
+    }
+
+    // if the request has a '.' assume that it's for a file, move along
+    var ext = path.extname(req.path);
+    if (ext !== ''){
+        return next();
+    }
+    fs.createReadStream(staticRoot + 'index.html').pipe(res);
 });
 
 app.listen(port, function () {
